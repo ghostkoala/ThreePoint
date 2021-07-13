e24dc0c575a96798a0bc5a2e556de8e035027854
ghostkoala <ghostkoala@gmail.com>
2021-07-13 18:16:23 +0800

    message3

---
+++ Logicore.Core/Filters/DropDownAdminFilter.cs
@@ -0,0 +1,15 @@
+using Logicore.Core.Enities.ServiceModel;
+
+namespace Logicore.Core.Filters
+{
+    /// <summary>
+    /// 下拉菜单用户过滤
+    /// </summary>
+    public class DropDownAdminFilter
+    {
+        /// <summary>
+        /// 搜索模式
+        /// </summary>
+        public DropDownOrSelectModel? model { get; set; }
+    }
+}
