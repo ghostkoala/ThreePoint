62d711dba487b4e2fb7c5e642ffb488403424479
ghostkoala <ghostkoala@gmail.com>
2021-11-15 18:02:27 +0800

    calcu

--- ThreePoint.E-commerce/wwwroot/js/product/Calculation.js
+++ ThreePoint.E-commerce/wwwroot/js/product/Calculation.js
@@ -1,42 +1,42 @@
 // 利润计算按钮事件
-function calcu(){
-    var PurchasePrice=$("#PurchasePrice");
+function calcu() {
+    var PurchasePrice = $("#PurchasePrice");
     if (PurchasePrice.val() == null || PurchasePrice.val() == "") {
         alert("请输入采购价！");
         PurchasePrice.focus();
         return false;
     }
-    var Pcs=$("#Pcs");
+    var Pcs = $("#Pcs");
     if (Pcs.val() == null || Pcs.val() == "") {
         alert("请输入货品数量！");
         Pcs.focus();
         return false;
     }
-    var SellingPrice=$("#SellingPrice");
+    var SellingPrice = $("#SellingPrice");
     if (SellingPrice.val() == null || SellingPrice.val() == "") {
         alert("请输入销售价！");
         SellingPrice.focus();
         return false;
     }
-    var ExchangeRate=$("#ExchangeRate");
+    var ExchangeRate = $("#ExchangeRate");
     if (ExchangeRate.val() == null || ExchangeRate.val() == "") {
         alert("请输入汇率！");
         ExchangeRate.focus();
         return false;
     }
-    var SalesCommission=$("#SalesCommission");
+    var SalesCommission = $("#SalesCommission");
     if (SalesCommission.val() == null || SalesCommission.val() == "") {
         alert("请输入销售佣金！");
         ExchangeRate.focus();
         return false;
     }
-    var FBAshipping=$("#FBAshipping");
+    var FBAshipping = $("#FBAshipping");
     if (FBAshipping.val() == null || FBAshipping.val() == "") {
         alert("请输入FBA费用！");
         FBAshipping.focus();
         return false;
     }
-    var ReturnAdvertisementPoint=$("#ReturnAdvertisementPoint");
+    var ReturnAdvertisementPoint = $("#ReturnAdvertisementPoint");
     if (ReturnAdvertisementPoint.val() == null || ReturnAdvertisementPoint.val() == "") {
         alert("请输入退货+广告费用率！");
         ReturnAdvertisementPoint.focus();
@@ -48,11 +48,11 @@ function calcu(){
         ReturnAdvertisementPoint.focus();
         return false;
     }
-    
+
     //console.log(SellingPrice);
     var Profit = (SellingPrice.val() * (1 - ReturnAdvertisementPoint.val() / 100) - SalesCommission.val() - FBAshipping.val()) * ExchangeRate.val() - PurchasePrice.val() - FreightCost.val()
     var totalProfit = Pcs.val() * Profit;
-    var Profitrate = parseInt((Profit / SellingPrice.val() * ExchangeRate.val()) * 100);
+    var Profitrate = parseInt((Profit / (SellingPrice.val() * ExchangeRate.val())) * 100);
 
     $("#profit").val(Profit.toFixed(2));
     $("#totalprofit").val(totalProfit.toFixed(2));
@@ -61,14 +61,40 @@ function calcu(){
 
 // 自动计算佣金
 $("#SellingPrice").change(function () {
-    var p= $("#SellingPrice").val() * 0.15;
-    $("#SalesCommission").val(p.toFixed(2)); 
+    var p = $("#SellingPrice").val() * 0.15;
+    $("#SalesCommission").val(p.toFixed(2));
 });
 
 // 选择货币事件
 $("#SellingCurrency").change(function () {
-    var s= $(this).find("option:selected").text();
+    var s = $(this).find("option:selected").text();
     console.log(s);
     $("#sc1").text(s);
     $("#sc2").text(s);
+});
+
+// 广告+退货金额事件
+$("#ReturnAdvertisementPoint").keyup(function () {
+    s = $("#SellingPrice").val();
+    if (s > 0) {
+        p = s * $(this).val() / 100;
+        $("#returnAdvLabel").text(p);
+    }
+    else {
+        $("#returnAdvLabel").text("");
+    }
+    return true;
+});
+
+// 销售价格事件
+$("#SellingPrice").keyup(function () {
+    s = $("#ReturnAdvertisementPoint").val();
+    if (s > 0) {
+        p = s * $(this).val() / 100;
+        $("#returnAdvLabel").text(p);
+    }
+    else {
+        $("#returnAdvLabel").text("");
+    }
+    return true;
 });
